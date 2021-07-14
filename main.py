fc8609629e911a2a0a7b03152707a37e0d189caf
john <john@jpatmore.plus.com>
2021-07-14 13:38:33 +0100

    Update to search ads on main page - no need to click through

--- main.py
+++ main.py
@@ -35,27 +35,8 @@ options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
 
-chrome_driver_path = "E:/Python/WebDriver/chromedriver.exe"
-driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
-
-url = "https://www.gumtree.com.au/"
-login_url = "https://www.gumtree.com.au/t-login-form.html"
-
-driver.get(url)
-time.sleep(5)
-
-
-def login():
-    driver.get(login_url)
-    time.sleep(5)
-    login_email = driver.find_element_by_id("login-email")
-    login_email.send_keys(USERNAME)
-    time.sleep(5)
-    login_password = driver.find_element_by_id("login-password")
-    login_password.send_keys(PASSWORD)
-    login_password.send_keys(Keys.ENTER)
-    time.sleep(5)
-
+# prepare message template
+message_template = "Hey you! Is this still available? Super keen. When do you need it gone by? Let's set a time :)"
 
 # search for items to message
 # create search list for items
@@ -90,67 +71,68 @@ excluded_words = ("broken",
                   "not working"
                   )
 
-page = 1
-free_url = "https://www.gumtree.com.au/sydney/page-" + str(page) + "l3003642r20?price-type=free"
 
-driver.get(free_url)
-driver.set_page_load_timeout(5)
+def login():
+    # url = "https://www.gumtree.com.au/"
+    # driver.get(url)
+    # time.sleep(5)
 
-# prepare message template
-message_template = "Hey you! Is this still available? Super keen. When do you need it gone by? Let's set a time :)"
+    login_url = "https://www.gumtree.com.au/t-login-form.html"
+    driver.get(login_url)
+    time.sleep(5)
+    login_email = driver.find_element_by_id("login-email")
+    login_email.send_keys(USERNAME)
+    time.sleep(5)
+    login_password = driver.find_element_by_id("login-password")
+    login_password.send_keys(PASSWORD)
+    login_password.send_keys(Keys.ENTER)
+    time.sleep(5)
 
 
-# search if description has keywords
-def selectAd():
-    count = 1
-    xPath = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div[3]/main/section/div[2]/div/a["
-                                         + str(count) + "]/div[2]/div/p")
-    xPath.click()
-    time.sleep(3)
+def search_ads():
+    results = []
+    ads_section = driver.find_element_by_xpath('//*[@id="react-root"]/div/div[3]/div/div[3]/main/section')
+    user_ad_list = ads_section.find_elements_by_tag_name('a')
 
     item_found = False
-    ad_container = driver.find_element_by_xpath(f"//*[@class='vip-ad__container']")
-    print("Searching through watch_list\n============================")
-    for word in watch_list:
-        try:
-            search_keywords = ad_container.find_element_by_xpath(f".//*[contains(text(), \"{word}\")]")
-            if search_keywords:
-                print(f'Found Keyword \"{word}\" in \"{search_keywords.text}\"')
+    for user_ad in user_ad_list:
+        ad_href = user_ad.get_attribute('href')
+        ad_title = user_ad.find_element_by_class_name('user-ad-row-new-design__title-span').text
+        ad_description = user_ad.find_element_by_class_name('user-ad-row-new-design__description-text').text
+
+        print("Searching through watch_list\n============================")
+        for word in watch_list:
+            if word in ad_title or word in ad_description:
+                print(f'Found Watch Word: \"{word}\"')
                 item_found = True
-        except exceptions.NoSuchElementException:
-            print(f"Keyword \"{word}\" not found")
+                break
 
-    print("\nSearching through excluded_words\n================================")
-    for word in excluded_words:
-        try:
-            search_keywords = ad_container.find_element_by_xpath(f".//*[contains(text(), \"{word}\")]")
-            if search_keywords:
-                print(f'Found Keyword \"{word}\" in \"{search_keywords.text}\"')
+        print("\nSearching through excluded_words\n================================")
+        for word in excluded_words:
+            if word in ad_title or word in ad_description:
+                print(f'Found Excluded Word: \"{word}\"')
                 item_found = False
-        except exceptions.NoSuchElementException:
-            print(f"Keyword \"{word}\" not found")
-    return item_found
+                break
+
+        if item_found:
+            results.append(
+                {
+                    'href': ad_href,
+                    'title': ad_title,
+                    'description': ad_description,
+                }
+            )
+
+    return results
 
 
-    if search_keywords != watch_list or search_keywords == excluded_words:
-        driver.back()
-        count += 1
-    else:
-        message_box = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div[3]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/form/div[1]/textarea")
-        message_box.send_keys(Keys.CONTROL + "a")
-        message_box.send_keys(message_template)
-        time.sleep(randint(1, 3))
-        send_message = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div[2]/div/div/div[2]/button")
-        send_message.click()
-        driver.back()
-        count += 1
+chrome_driver_path = "E:/Python/WebDriver/chromedriver.exe"
+driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
 
+driver.set_page_load_timeout(15)
 
-send_email = selectAd()
+url = "https://www.gumtree.com.au/s-electronics-computer/sydney/smart+tv/k0c20045l3003435?price=__20.00"
+driver.get(url)
 
-# change search results to max
-
-# run all functions parallel
-
-
-# driver.quit()
+search_results = search_ads()
+print(search_results)
